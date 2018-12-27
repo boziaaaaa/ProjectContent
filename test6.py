94a479b7ec05958fe2a4ef6313b2f62065b900fb
boziaaaaa <42334110+boziaaaaa@users.noreply.github.com>
2018-12-27 17:18:24 +0800

    Delete test6.py

--- test6.py
+++
@@ -1,8 +0,0 @@
-import numpy
-from PIL import Image
-import os
-inputFile = "C:\Users\\bozi\Documents\Downloads\cloudRatio201802_txt\\"
-
-files = os.listdir(inputFile)
-for i in files:
-    print inputFile + i
