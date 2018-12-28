c7566cd727660164078a2cbb787e69c1eea5b80d
bozi <boziaaaaa@163.com>
2018-12-28 11:28:55 +0800

    add different merge functions

--- picture/ImageMerge_transparent.py
+++ picture/ImageMerge_transparent.py
@@ -1,3 +1,4 @@
+#coding=utf8
 from PIL import Image
 import numpy
 import matplotlib.pyplot as plt
@@ -27,7 +28,6 @@ def ImageMerge(inputPicture1,inputPicture2,leftRightCorner):
     img_fore = Image.open(inputPicture2)
     img_fore = MakeImageTransparent(img_fore)
     r,g,b,a = img_fore.split()
-    # img_back.paste(img_fore,(100, 100, 740,580),mask=a)
     imageSize = img_fore.size
     left = leftRightCorner[0]
     up = leftRightCorner[1]
@@ -35,10 +35,35 @@ def ImageMerge(inputPicture1,inputPicture2,leftRightCorner):
     down = up + imageSize[1]
     img_back.paste(img_fore,(left,up,right,down),mask=a)
     img_back.show()
+def ImageMerge2(inputPicture1,inputPicture2,leftRightCorner):
+    img_back = Image.open(inputPicture1)
+    img_back = img_back.convert("RGBA")
+    img_fore = Image.open(inputPicture2)
+    img_fore = img_fore.convert("RGBA")
+    img_fore = img_fore.resize(img_back.size)
+    img = Image.blend(img_back,img_fore,0.5)
+    img.show()
 
+def ImageMerge3(inputPicture1, inputPicture2, leftRightCorner):
+    img_back = Image.open(inputPicture1)
+    img_back = img_back.convert("RGBA")
+    img_fore = Image.open(inputPicture2)
+    img_fore = img_fore.convert("RGBA")
+    img_fore = img_fore.resize(img_back.size)
+    r,g,b,a = img_fore.split()
+    a_array = numpy.array(a)
+    print a_array
+    img = Image.composite(img_back, img_fore, a)
+    img.show()
 if __name__=="__main__":
     inputPicture1 = "D:\ProjectContent\\test\FY4A-_AGRI--_N_REGI_1047E_L1C_MTCC_MULT_GLL_20181213050000_20181213051459_1000M_V0001.JPG"
     inputPicture2 = "D:\ProjectContent\\test\\0001.JPG"
+    inputPicture3 = "D:\ProjectContent\\test\\daodao.JPG"
     # MakePicture(inputPicture2)
     leftRightCorner = [1000,1000]
-    ImageMerge(inputPicture1,inputPicture2,leftRightCorner)
+    ImageMerge(inputPicture1,inputPicture1,leftRightCorner)
+
+    #two image must have the same size
+    # ImageMerge2(inputPicture1,inputPicture3,leftRightCorner)
+    #two image must have the same size
+    ImageMerge3(inputPicture1,inputPicture3,leftRightCorner)
