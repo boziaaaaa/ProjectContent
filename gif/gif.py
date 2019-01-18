5645347c719fc7345966298244c7979b3a3492a1
bozi <boziaaaaa@163.com>
2019-01-18 16:10:52 +0800

    201901

--- gif/gif.py
+++ gif/gif.py
@@ -2,45 +2,46 @@ import imageio
 import os
 import sys
 from PIL import Image
-import matplotlib.pyplot as plt
-def ParseGIF(inputGIF,outputPath):
-    tif = imageio.read(inputGIF)
-    index = 0
-    for i in tif:
-        print i.shape
-        img = Image.fromarray(i)
-        index += 1
-        img.save(inputGIF.replace(".gif",str(index).zfill(3)+".png"))
-    tif.close
-def MakeGIF(inputPNGs,outputFile):
-    files_gif = []
-    for file in inputPNGs:
-        # img = Image.open(file)
-        # plt.imshow(img)
-        # fileBasename = os.path.basename(file)
-        # date = fileBasename.split("_")
-        # title =  "EVT_" + date[9] + "_" + date[10]
-        # plt.title(title)
-        # plt.show()
-        gif_each = imageio.imread(file)
-        files_gif.append(gif_each)
-    imageio.mimsave(outputFile,files_gif,"GIF",duration = 0.1)
-if __name__=="__main__":
-    inputGIF = r"D:\ProjectContent\gif\ce4.gif"
-    outputPath = os.path.dirname(inputGIF)
-    inputPNGpath = os.path.dirname(inputGIF)
-    inputPNGpath = r"D:\Data_ftp\20190110"
+class GIF(object):
+    def __init__(self,t=0.1):
+        self.timeResolution = t
 
+    def ParseGIF(self,inputGIF,outputPath):
+        tif = imageio.read(inputGIF)
+        index = 0
+        for i in tif:
+            print i.shape
+            img = Image.fromarray(i)
+            index += 1
+            img.save(inputGIF.replace(".gif",str(index).zfill(3)+".png"))
+        tif.close
 
+    def MakeGIF(self,inputPNGs,outputFile):
+        files_gif = []
+        for file in inputPNGs:
+            gif_each = imageio.imread(file)
+            files_gif.append(gif_each)
+        imageio.mimsave(outputFile,files_gif,"GIF",duration = self.timeResolution)
+
+def GetPNGS(inputPNGpath):
     files = os.listdir(inputPNGpath)
     pngs = []
-    outputFile = os.path.join(inputPNGpath,"test_evt.gif")
     for file in files:
-        if "EVT" in file and "TOTAL" in file:
-            pngs.append(os.path.join(inputPNGpath,file))
+        # if "EVT" in file and "TOTAL" in file:
+        if "LAD" in file :
+            pngs.append(os.path.join(inputPNGpath, file))
     if len(pngs) == 0:
         print "no pngs exist!!"
         sys.exit()
-    MakeGIF(pngs,outputFile)
+    return pngs
+if __name__=="__main__":
+    timeResolution = 0.02
+    inputDate = "20190117"
+    inputPNGpath = r"D:\Data_ftp\\"+inputDate
+    outputFile = r"D:\Data_ftp\gif_test\lad_"+inputDate+"_timeResolution_"+str(timeResolution)+"s.gif"
+
+    pngs = GetPNGS(inputPNGpath)
+    makeGif = GIF(timeResolution)
+    makeGif.MakeGIF(pngs,outputFile)
 
 
