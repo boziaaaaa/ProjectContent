2b474d652b17ce086a96890c6323a41aa6c0f29a
bozi <boziaaaaa@163.com>
2019-03-01 10:45:52 +0800

    201901

--- spider/IE_fake.py
+++ spider/IE_fake.py
@@ -2,13 +2,11 @@
 import time
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-headers={
-    "user-agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36"
-}
+
 options = webdriver.ChromeOptions()
 options.add_argument('user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.119 Safari/537.36')
 html = webdriver.Chrome(r"C:\Program Files (x86)\Google\Chrome\Application\chromedriver.exe",options=options)
-html.get('www.baidu.com')
+html.get('http://www.baidu.com')
 print(html.title)
 elem = html.find_element_by_id('kw')
 elem.send_keys("python")
